ee54714633850ce29c10badb73fff4b77ccaf713
BlastedLattic021 <139406635+RicardoDuranS@users.noreply.github.com>
2024-11-24 18:03:06 -0600

    Traffic lights inteligence

--- TrafficLightAgent.py
+++ TrafficLightAgent.py
@@ -2,31 +2,40 @@ import mesa
 
 
 class TrafficLightAgent(mesa.Agent):
-    def __init__(self, unique_id, state, model, group=None):
-        """
-        Initialize a TrafficLightAgent.
-
-        Args:
-            unique_id (str): Unique identifier for the traffic light.
-            state (str): Initial state of the traffic light ("red" or "green").
-            model (Model): The model instance that contains this agent.
-        """
+    def __init__(
+        self, traffic_light_id, state, model, group, position, monitored_positions
+    ):
         super().__init__(model)
-        self.unique_id = unique_id
+        self.traffic_light_id = traffic_light_id
         self.state = state
         self.time_counter = 0
         self.group = group
+        self.position = position
+        self.monitored_positions = monitored_positions
 
-    def change_state(self, state):
+    def change_state(self, new_state):
         """Change the state of the traffic light."""
-        self.state = state
+        self.state = new_state
+
+    def check_cars(self):
+        """Check if there are cars in the monitored positions."""
+        in_area_cars = 0
+        for pos in self.monitored_positions:
+            agents = self.model.grid.get_cell_list_contents([pos])
+            for agent in agents:
+                if isinstance(agent, CarAgent):
+                    in_area_cars += 1
+        return in_area_cars
+
+    def sync_with_group(self):
+        """Synchronize the state of the traffic light with the group."""
+        for pos in self.position:
+            agents = self.model.grid.get_cell_list_contents([pos])
+            for agent in agents:
+                if isinstance(agent, TrafficLightAgent) and agent.group == self.group:
+                    agent.change_state(self.state)
 
     def step(self):
-        """
-        Advance the traffic light by one step.
-
-        The traffic light changes state every 30 steps.
-        """
         self.time_counter += 1
         if self.time_counter >= 10:
             self.change_state()
