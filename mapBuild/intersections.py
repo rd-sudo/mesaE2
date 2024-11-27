b90608511c2f9bc1f0c0876fbbd70e7e75f19d7a
nicoleKape-code <110926449+nicoleKapellmann@users.noreply.github.com>
2024-11-27 07:46:36 -0600

    Commit

---
+++ mapBuild/intersections.py
@@ -0,0 +1,77 @@
+monitoring_coors = {
+    1:{
+        "pos": (1,9),
+        "area":[[(x, 9) for x in range(2, 12)], [(x, 8) for x in range(2, 12)]],
+        "direction":"right", 
+    },
+    2:{
+        "pos": (1,8),
+        "area":[[(x, 9) for x in range(2, 12)], [(x, 8) for x in range(2, 12)]],
+        "direction":"right",
+    },
+    3:{
+        "pos": (6, 11),
+        "area":[[(6, y) for y in range(12, 22)], [(7, y) for y in range(12, 22)]],
+        "direction":"up",
+    },
+    4:{
+        "pos": (7,11),
+        "area":[[(6, y) for y in range(12, 22)], [(7, y) for y in range(12, 22)]],
+        "direction":"up",
+    },
+    5:{
+        "pos": (6,8),
+        "area":[[(6, y) for y in range(2, 8)], [(7, y) for y in range(2, 8)]],
+        "direction":"down",
+    },
+    6:{
+        "pos": (7,8),
+        "area":[[(6, y) for y in range(2, 8)], [(7, y) for y in range(2, 8)]],
+        "direction":"down",
+    },
+    7:{
+        "pos": (6,5),
+        "area":[[(x, 5) for x in range(2, 6)], [(x, 4) for x in range(2, 6)]],
+        "direction":"left",
+    },
+    8:{
+        "pos": (6,4),
+        "area":[[(x, 5) for x in range(2, 6)], [(x, 4) for x in range(2, 6)]],
+        "direction":"left",
+    },
+    9:{
+        "pos": (7,5),
+        "area":[[(x, 5) for x in range(8, 12)], [(x, 4) for x in range(8, 12)]],
+        "direction":"right",
+    },
+    10:{
+        "pos": (7,4),
+        "area":[[(x, 5) for x in range(8, 12)], [(x, 4) for x in range(8, 12)]],
+        "direction":"right",
+    },
+    11:{
+        "pos": (12,22),
+        "area":[[(12, y) for y in range(0, 22)], [(13, y) for y in range(0, 22)]],
+        "direction":"down",
+    },
+    12:{
+        "pos": (13,22),
+        "area":[[(12, y) for y in range(0, 22)], [(13, y) for y in range(0, 22)]],
+        "direction":"down",
+    },
+    13:{
+        "pos": (12,18),
+        "area":[[(x, 18) for x in range(8, 12)], [(x, 17) for x in range(8, 12)]],
+        "direction":"left",
+    },
+    14:{
+        "pos": (12,17),
+        "area":[[(x, 18) for x in range(8, 12)], [(x, 17) for x in range(8, 12)]],
+        "direction":"left",
+    },
+    15:{
+        "pos": (12,11),
+        "area":[[(x, 11) for x in range(2, 12)], [(x, 10) for x in range(2, 12)]],
+        "direction":"left",
+    },
+}
